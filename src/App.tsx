8f420fc45bb5c5c1125914015a01685beeb7c977
aaronhalder-wisecode <aaron@wisecodetech.com>
2024-04-29 16:42:53 +0600

    Built a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-  return <Button colorScheme="blue">Button</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"header"
+        "main"`,
+        lg: `"header header"
+        "aside main"`,
+      }}
+    >
+      <GridItem bg="orange.300" area={"header"}>
+        Header
+      </GridItem>
+      <Show above="lg">
+        <GridItem bg="pink.300" area={"aside"}>
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem bg="green.300" area={"main"}>
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
